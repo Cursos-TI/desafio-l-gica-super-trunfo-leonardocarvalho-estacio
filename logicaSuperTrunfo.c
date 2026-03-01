13acce269fa3a121b58da738ff3037600248a2f2
leonardocarvalho-estacio <202510142621@alunos.estacio.br>
2026-03-01 19:16:42 +0000

    Colei as partes relevantes do código do tema 2 para começar a desenvolver

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -9,17 +9,84 @@ int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
+    int numero_pontos_turisticos1, numero_pontos_turisticos2;
+    unsigned long int populacao1, populacao2;
+    float super_poder1, super_poder2, area_cidade1, pib1, area_cidade2, pib2, densidade_populacional1 = 0, densidade_populacional2 = 0, pib_per_capita1 = 0, pib_per_capita2 = 0;
+    char estado1, codigo_carta1[20], nome_cidade1[50], estado2, codigo_carta2[20], nome_cidade2[50];
+
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
     // utilizando a função scanf para capturar as entradas.
     // utilize o código do primeiro desafio
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    printf("Digite os dados de duas cartas para o jogo Super Trunfo de Paises.\n");
+
+    // Solicitação dos dados da primeira carta
+
+    printf("\nDigite o Estado da primeira carta: ");
+    scanf(" %c", &estado1);
+
+    printf("\nDigite o codigo da primeira carta: ");
+    scanf(" %s", &codigo_carta1);
+    getchar();
+
+    printf("\nDigite o nome da cidade da primeira carta: ");
+    fgets(nome_cidade1, 50, stdin);
+
+    printf("\nDigite o numero de habitantes da cidade da primeira carta: ");
+    scanf(" %i", &populacao1);
+
+    printf("\nDigite a area em quilometros quadrados da cidade da primeira carta: ");
+    scanf(" %f", &area_cidade1);
+
+    printf("\nDigite o PIB da cidade da primeira carta: ");
+    scanf(" %f", &pib1);
+
+    printf("\nDigite o numero de pontos turisticos da cidade da primeira carta: ");
+    scanf(" %i", &numero_pontos_turisticos1);
+
+    printf("\n\n");
+
+    // Calculando densidade populacional, super poder e pib per capita da carta 1
+
+    densidade_populacional1 = populacao1/area_cidade1;
+    pib_per_capita1 = pib1/populacao1;
+    super_poder1 = ( (float) populacao1 + area_cidade1 + pib1 + (float) numero_pontos_turisticos1 + pib_per_capita1 - densidade_populacional1 );
+
+    // Solicitação dos dados da segunda carta
+
+    printf("\nAgora serao requisitados os dados da segunda carta.\n");
+    
+    printf("\nDigite o Estado da segunda carta: ");
+    scanf(" %c", &estado2);
+
+    printf("\nDigite o codigo da segunda carta: "); 
+    scanf(" %s", &codigo_carta2);
+    getchar();
+
+    printf("\nDigite o nome da cidade da segunda carta: ");
+    fgets(nome_cidade2, 50, stdin);
+
+    printf("\nDigite o numero de habitantes da cidade da segunda carta: ");
+    scanf(" %i", &populacao2);
+
+    printf("\nDigite a area em quilometros quadrados da cidade da segunda carta: ");
+    scanf(" %f", &area_cidade2);
+
+    printf("\nDigite o PIB da cidade da segunda carta: ");
+    scanf(" %f", &pib2);
+
+    printf("\nDigite o numero de pontos turisticos da cidade da segunda carta: ");
+    scanf(" %i", &numero_pontos_turisticos2);
+
+    printf("\n\n");
+
+    // Calculando densidade populacional, super poder e pib per capita da carta 2
+
+    densidade_populacional2 = populacao2/area_cidade2;
+    pib_per_capita2 = pib2/populacao2;
+    super_poder2 = ( (float) populacao2 + area_cidade2 + pib2 + (float) numero_pontos_turisticos2 + pib_per_capita2 - densidade_populacional2 );
 
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
